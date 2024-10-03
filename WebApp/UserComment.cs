69d070afe502ebdda8e9e9affe4842b40388a68f
Abiel <97120759+abielinsky@users.noreply.github.com>
2024-10-03 12:44:12 +0100

    Add project files.

---
+++ WebApp/UserComment.cs
@@ -0,0 +1,53 @@
+﻿using WebApp.services;
+
+namespace WebApp
+{
+    public class UserComment
+    {
+
+        public string RawComment { get; set; }
+        public string SanitizedComment { get; set; }
+
+        public SanitizeService sanitizer = new SanitizeService();
+        public UserComment(string rawComment) 
+        {
+            RawComment = rawComment;
+            SanitizedComment = sanitizer.SanitizeText(rawComment);
+        }
+
+    }
+
+    //XML GENERATE BY... COPY FROM THE URL API, THEN COPY, EDIT, SPECIAL PASTE, XML AS CLASES
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.purgomalum.com")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.purgomalum.com", IsNullable = false)]
+    public partial class PurgoMalum
+    {
+
+        private string resultField;
+
+        /// <remarks/>
+        public string result
+        {
+            get
+            {
+                return this.resultField;
+            }
+            set
+            {
+                this.resultField = value;
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ WebApp/services/SanitizeService.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System.Xml.Serialization;
+
+namespace WebApp.services
+{
+    public class SanitizeService
+    {
+        static readonly string SANITIZE_URL = "http://www.purgomalum.com/service/xml";
+
+        public string SanitizeText(string rawValue)
+        {
+            var client = new RestClient(SANITIZE_URL);
+            var request = new RestRequest();
+            request.AddParameter("text", rawValue);
+            var response = client.Execute(request);
+
+            if (!string.IsNullOrWhiteSpace(response.Content))
+            {
+                // Code to deserialize content XML here
+                var serializer = new XmlSerializer(typeof(PurgoMalum));
+                using (StringReader sr = new StringReader(response.Content))
+                {
+                    PurgoMalum? filtered = (PurgoMalum?)serializer.Deserialize(sr);
+                    return filtered.result;
+                }
+            }
+
+            return string.Empty;
+        }
+
+
+    }
+}
